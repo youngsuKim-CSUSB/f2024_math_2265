ab097a5bfabf74c6644e9070c29d7968beaef1de
youngsuKim-CSUSB <81013158+youngsuKim-CSUSB@users.noreply.github.com>
2024-09-11 19:07:59 -0700

    add draft

---
+++ examples/ch2-2_categorical_variables.R
@@ -0,0 +1,12 @@
+library("openintro")
+library("tidyverse")
+
+## Contingency Table (two-way table)
+
+## bar plot of single variable
+
+## prop table
+
+## bar plot of two variables
+
+## mosaic plot

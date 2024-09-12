63403bf782637a919ca31b6b7a69fa74a276ad04
youngsuKim-CSUSB <81013158+youngsuKim-CSUSB@users.noreply.github.com>
2024-09-12 15:11:58 -0700

    add worksheet

--- examples/ch2-2_categorical_variables.R
+++ examples/ch2-2_categorical_variables.R
@@ -1,7 +1,7 @@
 library("openintro")
-library("tidyverse")
+# library("tidyverse")
 
-## Contingency Table (two-way table)
+## Contingency Table (two-way table): use `loan50` or `loans_full_schema` 
 
 ## bar plot of single variable
 
@@ -10,3 +10,13 @@ library("tidyverse")
 ## bar plot of two variables
 
 ## mosaic plot
+
+## Check your understanding 
+
+## categorical variables
+## (1) Grab two categorical variables and make a contingency table and bar plots 
+## (2)What can you say about the results?
+
+## numerical variable
+## (1) make two vectors (of length 5) having the same median but different min (or max)
+## (1) change of the vectors such that its mean is the same but the standard deviations are different

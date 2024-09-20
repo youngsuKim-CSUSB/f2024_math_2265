4da92792e0ff4d7ba8090d499e4e11be2eea8817
youngsuKim-CSUSB <81013158+youngsuKim-CSUSB@users.noreply.github.com>
2024-09-20 16:12:09 -0700

    init commit

---
+++ examples/suggested_answers/9-10-r_worksheet_your_name.R
@@ -0,0 +1,45 @@
+## First load required libraries 
+library(openintro)
+library(ggplot2)
+
+# In each question, replace <> by your answer. 
+
+# Question 1. 
+# (1) Make a scatter plot of the variables `debt_to_income` (for x) and `total_credit_utilized` (for y) in `loan50`.
+ggplot(data=<>, map=aes(x=<>, y=<>)) +
+  geom_point()
+
+# (2) What kind of associations do they have?
+# Ans: 
+
+# Question 2. 
+# (1) Make a histogram of the variable `annual_income` in `loan50`.
+ggplot(data=<>, map=aes(x=<>)) +
+  geom_histogram()
+
+# (2) Is it symmeric, skewed-left, skewed-right?
+# Ans:
+
+# Question 3. Consider the variable `num_cc_carrying_balance` in `loan50`.
+# (1) Save the sorted loan50$num_cc_carrying_balance and to the variable my_var.
+my_var <- sort(<>)
+
+# (2) Find Q1, median, and Q3 
+median(my_var)
+
+q_lower <- <>
+
+median(q_lower)  
+
+q_upper <- <>
+  
+median(q_upper)
+
+# (3) Make a boxplot of the variable `num_cc_carrying_balance` in `loan50`
+ggplot(data=<>, map=aes(x=<>)) +
+  geom_boxplot()
+
+# (4) Are there any outliers?
+
+# Question 4. Save your work as 9-10-r_worksheet_your_name.R and upload it to Canvas: 
+# https://csusb.instructure.com/courses/42324/assignments/670837
---
+++ examples/suggested_answers/9-17-worksheet_your_name.R
@@ -0,0 +1,50 @@
+## In-class exercise 
+## Make a group of 2-3 people and work these are together
+## It is okay that one person writes the code and share it with other group members (e.g. via email)
+## Your work is evaluated mainly my completion and partipation 
+
+## load the packages 
+library("openintro")
+library("ggplot2")
+
+## Question 1
+my_vec <- c( 15, 8, 7, 2, 19, 3, 11)
+my_vec <- sort(my_vec)
+my_vec
+
+### (a) Compute the mean and median
+
+### (b) Compute the standard deviation by using the formula and compare the result with sd(my_vec)
+sd(my_vec)
+
+### (c) Make the box plot of my_vec: the following line creates a data frame consisting of the variable my_vec
+my_data <- data.frame(my_vec)
+ggplot(data=my_data, aes(x=my_vec)) + 
+  geom_<>
+
+### (d) Does this data have any outliers?
+  
+## Question 2
+## The following lines will update the max in `my_vec` to 61.
+  
+my_vec2 <- my_vec
+my_vec2[7] <- 61
+my_vec2
+
+### (a) Make the box plot of my_vec2:
+my_data2 <- data.frame(my_vec2)
+ggplot(data=my_data, aes(x=my_vec2)) + 
+  geom_<>
+
+### (b) Does this data have any outliers?
+
+## Question 3
+  
+### (a) Which data set has bigger mean? Use R to verify your reasoning
+  
+### (b) Which data set has bigger median? Use R to verify your reasoning
+  
+### (c) A typical observation in my_vec2 is best represented by the 
+### mean or median?
+
+## Be sure to share your answers and upload your file to Canvas
